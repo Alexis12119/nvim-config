fcd0b7d49289a72f4de7920558a84edf6f491970
Alexis <corporal461@gmail.com>
2023-04-13 01:01:45 +0800

    feat(theme): use habamax when the theme can't be found

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ local name = "nightly"
 local theme_ok = pcall(vim.cmd.colorscheme, name)
 if not theme_ok then
   vim.notify("The theme isn't installed or you had a typo", vim.log.levels.ERROR)
+  vim.cmd.colorscheme("habamax")
 else
   require("plugins.theme." .. name)
 end
