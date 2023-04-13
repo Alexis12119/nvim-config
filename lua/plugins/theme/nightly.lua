d6156eebcb9b2d444282d4632c2ca930e692f1a5
Alexis <corporal461@gmail.com>
2023-04-13 10:29:43 +0800

    feat(theme): use `black` and cleanup

--- lua/plugins/theme/nightly.lua
+++ lua/plugins/theme/nightly.lua
@@ -1,18 +1,6 @@
--- Set color style by time
--- local color_by_time = function()
---   local time = tonumber(os.date "%H")
---   if time >= 6 and time <= 17 then
---     return "green"
---   elseif time >= 18 and time <= 23 then
---     return "blue"
---   elseif time >= 0 and time <= 5 then
---     return "black"
---   end
--- end
-
 require("nightly").setup {
-  color = "green", -- black, blue, green or red
-  transparent = true,
+  color = "black", -- black, blue, green or red
+  transparent = false,
 }
 
 vim.cmd "colorscheme nightly"
