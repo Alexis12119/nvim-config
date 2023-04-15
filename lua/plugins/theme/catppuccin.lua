71a854b65ef2e14c962a7ec8d21ed6b246eb1607
Alexis <corporal461@gmail.com>
2023-04-15 23:34:02 +0800

    feat(theme): change config for `catppuccin`

--- lua/plugins/theme/catppuccin.lua
+++ lua/plugins/theme/catppuccin.lua
@@ -1,4 +1,8 @@
 require("catppuccin").setup {
+  background = {
+    light = "latte",
+    dark = "mocha",
+  },
   transparent_background = false,
   term_colors = false,
   compile = {
@@ -24,8 +28,16 @@ require("catppuccin").setup {
     types = {},
     operators = {},
   },
+  color_overrides = {},
+  custom_highlights = {},
   integrations = {
-    -- For various plugins integrations see https://github.com/catppuccin/nvim#integrations
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    telescope = true,
+    notify = false,
+    mini = false,
+    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
   },
 }
 
