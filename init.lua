77179a4da858c55ceeeeb3de365c5483d199f92d
Alexis <corporal461@gmail.com>
2023-04-16 15:39:57 +0800

    feat(theme): use pcall

--- init.lua
+++ init.lua
@@ -8,5 +8,5 @@ if not theme_ok then
   vim.notify("The theme isn't installed or you had a typo", vim.log.levels.ERROR)
   vim.cmd.colorscheme "habamax"
 else
-  require("plugins.theme." .. name)
+  pcall(require, "plugins.theme." .. name)
 end
