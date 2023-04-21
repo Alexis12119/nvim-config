9bcf18d9dc8ceb5c027f31453148dff6f474ad3d
Alexis <corporal461@gmail.com>
2023-04-21 12:37:28 +0800

    feat(theme): use this function again to change colors

--- lua/plugins/theme/nightly.lua
+++ lua/plugins/theme/nightly.lua
@@ -1,5 +1,17 @@
+-- Set color style by time
+local color_by_time = function()
+  local time = tonumber(os.date "%H")
+  if time >= 6 and time <= 17 then
+    return "green"
+  elseif time >= 18 and time <= 23 then
+    return "blue"
+  elseif time >= 0 and time <= 5 then
+    return "black"
+  end
+end
+
 require("nightly").setup {
-  color = "black", -- black, blue, green or red
+  color = color_by_time(), -- black, blue, green or red
   transparent = false,
   styles = {
     comments = { italic = true },
