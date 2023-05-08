0c83173d23b97cabdfe474dd21fc96c4706d07df
Alexis <corporal461@gmail.com>
2023-05-09 06:26:32 +0800

    fix(keymaps): keymap setter

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -26,21 +26,19 @@ function _G.format_code()
 end
 
 function _G.set_keymaps(keymaps, mode)
-  local opt = { silent = true }
-
-  if mode == "c" then
-    opt = { expr = true }
-  end
-
   for keymap, value in pairs(keymaps) do
     if value.desc ~= nil then
       if value.opt ~= nil then
         vim.keymap.set(mode, keymap, value.cmd, value.opt, { desc = value.desc })
       else
-        vim.keymap.set(mode, keymap, value.cmd, { desc = value.desc })
+        local opt = { silent = true }
+
+        if mode == "c" then
+          opt = { expr = true }
+        end
+
+        vim.keymap.set(mode, keymap, value.cmd, opt, { desc = value.desc })
       end
-    else
-      vim.keymap.set(mode, keymap, value.cmd, opt)
     end
   end
 end
