729e7755c6ee78b4505c4fb0f83c7cd45b0293b4
Alexis <corporal461@gmail.com>
2023-05-11 18:09:43 +0800

    feat(lazy): change theme.

--- lua/plugin-loader.lua
+++ lua/plugin-loader.lua
@@ -31,7 +31,7 @@ local opts = {
     -- install missing plugins on startup. This doesn't increase startup time.
     missing = true,
     -- try to load one of these colorschemes when starting an installation during startup
-    colorscheme = { "everblush", "habamax" },
+    colorscheme = { "nightly", "habamax" },
   },
   ui = {
     -- a number <1 is a percentage., >1 is a fixed size
