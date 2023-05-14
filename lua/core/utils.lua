1617ad41e5124cab037b51a4b9f8ccb5a33786f7
Alexis <corporal461@gmail.com>
2023-05-15 05:01:59 +0800

    feat(which-key): enable more presets.

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -132,13 +132,13 @@ function _G.which_key_add(mappings, mode)
       -- the presets plugin, adds help for a bunch of default keybindings in Neovim
       -- No actual key bindings are created
       presets = {
-        operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
+        operators = true, -- adds help for operators like d, y, ... and registers them for motion / text object completion
         motions = true, -- adds help for motions
         text_objects = true, -- help for text objects triggered after entering an operator
         windows = true, -- default bindings on <c-w>
         nav = true, -- misc bindings to work with windows
         z = true, -- bindings for folds, spelling and others prefixed with z
-        g = false, -- bindings for prefixed with g
+        g = true, -- bindings for prefixed with g
       },
     },
     -- add operators that will trigger motion and text object completion
