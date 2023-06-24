e9376f539e94458bbc8a78249007997a323d9c70
Alexis12119 <corporal461@gmail.com>
2023-06-24 12:15:11 +0800

    feat(lualine): change theme style.

--- lua/plugins/lualine/theme.lua
+++ lua/plugins/lualine/theme.lua
@@ -1,33 +1,33 @@
 local theme = {
   normal = {
     a = { fg = "#000000", bg = "#7AA2F7" },
-    b = { bg = "NONE", fg = "#BBC2CF" },
-    c = { bg = "NONE", fg = "#BBC2CF" },
+    b = { bg = "#0F1416", fg = "#5A5D61" },
+    c = { bg = "NONE", fg = "#5A5D61" },
   },
 
   insert = {
     a = { fg = "#000000", bg = "#98BE65" },
-    b = { bg = "NONE", fg = "#BBC2CF" },
+    b = { bg = "#0F1416", fg = "#5A5D61" },
   },
 
   command = {
     a = { fg = "#000000", bg = "#ECBE7B" },
-    b = { bg = "NONE", fg = "#BBC2CF" },
+    b = { bg = "#0F1416", fg = "#5A5D61" },
   },
 
   visual = {
     a = { fg = "#000000", bg = "#C678DD" },
-    b = { bg = "NONE", fg = "#BBC2CF" },
+    b = { bg = "#0F1416", fg = "#5A5D61" },
   },
 
   replace = {
     a = { fg = "#000000", bg = "#DB4B4B" },
-    b = { bg = "NONE", fg = "#BBC2CF" },
+    b = { bg = "#0F1416", fg = "#5A5D61" },
   },
 
   terminal = {
     a = { fg = "#000000", bg = "#7AA2F7" },
-    b = { bg = "NONE", fg = "#BBC2CF" },
+    b = { bg = "#0F1416", fg = "#5A5D61" },
   },
 
   inactive = {
