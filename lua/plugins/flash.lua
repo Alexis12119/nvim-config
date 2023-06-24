baf3858f481c311bec1498d6371f81ee9842ff98
Alexis12119 <corporal461@gmail.com>
2023-06-24 11:04:06 +0800

    feat(flash): disable `char`.

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -101,7 +101,7 @@ return {
       -- options used when flash is activated through
       -- `f`, `F`, `t`, `T`, and `,` motions
       char = {
-        enabled = true,
+        enabled = false,
         -- by default all keymaps are enabled, but you can disable some of them,
         -- by removing them from the list.
         keys = { "f", "F", "t", "T", "," },
