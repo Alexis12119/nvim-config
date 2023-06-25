b0361785e4a1c07b0bb315d5003394d08a761ad7
Alexis12119 <corporal461@gmail.com>
2023-06-25 13:38:30 +0800

    feat(noice): use `hover` and `signature_help`.

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -34,13 +34,13 @@ M.setup = function()
 
   vim.diagnostic.config(config)
 
-  vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded",
-  })
+  -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  --   border = "rounded",
+  -- })
 
-  vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = "rounded",
-  })
+  -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  --   border = "rounded",
+  -- })
 end
 
 local function lsp_keymaps(bufnr)
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -21,13 +21,13 @@ return {
           ["cmp.entry.get_documentation"] = false,
         },
         hover = {
-          enabled = false,
+          enabled = true,
           silent = false, -- set to true to not show a message if hover is not available
           view = nil, -- when nil, use defaults from documentation
           opts = {}, -- merged with defaults from documentation
         },
         signature = {
-          enabled = false,
+          enabled = true,
           auto_open = {
             enabled = true,
             trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
