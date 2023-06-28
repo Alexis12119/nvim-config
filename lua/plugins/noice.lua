1da752aebe3b51000ad4120a8a5a214f0d70d59f
Alexis12119 <corporal461@gmail.com>
2023-06-28 16:22:04 +0800

    feat(noice): enable some presets opt.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -55,6 +55,15 @@ return {
           },
         },
       },
+      presets = {
+        -- you can enable a preset by setting it to true, or a table that will override the preset config
+        -- you can also add custom presets that you can enable/disable with enabled=true
+        bottom_search = true, -- use a classic bottom cmdline for search
+        command_palette = true, -- position the cmdline and popupmenu together
+        long_message_to_split = false, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false, -- add a border to hover docs and signature help
+      },
     }
   end,
   dependencies = {
