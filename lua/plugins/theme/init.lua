1e990255c1b5ec78c1d735c6a5425a0c5373ebaa
Alexis12119 <corporal461@gmail.com>
2023-07-12 16:18:20 +0800

    feat(theme): :)

--- lua/plugins/theme/init.lua
+++ lua/plugins/theme/init.lua
@@ -5,43 +5,43 @@ return {
     priority = 1000,
   },
 
-  -- {
-  --   "marko-cerovac/material.nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  --   config = function()
-  --     vim.g.material_style = "deep ocean"
-  --   end,
-  -- },
+  {
+    "marko-cerovac/material.nvim",
+    lazy = true,
+    priority = 1000,
+    config = function()
+      vim.g.material_style = "deep ocean"
+    end,
+  },
 
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  -- },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    priority = 1000,
+  },
 
-  -- {
-  --   "catppuccin/nvim",
-  --   lazy = true,
-  --   name = "catppuccin",
-  --   priority = 1000,
-  -- },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    priority = 1000,
+  },
 
-  -- {
-  --   "navarasu/onedark.nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  -- },
+  {
+    "navarasu/onedark.nvim",
+    lazy = true,
+    priority = 1000,
+  },
 
-  -- {
-  --   "rebelot/kanagawa.nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  -- },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    priority = 1000,
+  },
 
-  -- {
-  --   "Mofiqul/dracula.nvim",
-  --   lazy = true,
-  --   priority = 1000,
-  -- },
+  {
+    "Mofiqul/dracula.nvim",
+    lazy = true,
+    priority = 1000,
+  },
 }
