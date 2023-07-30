e394c853654164f8ebe7d9d1009449a35571d1dd
Alexis12119 <corporal461@gmail.com>
2023-07-30 16:39:46 +0800

    feat(vsc): check.

--- init.lua
+++ init.lua
@@ -1,11 +1,15 @@
-local name = "nightly"
+if vim.g.vscode then
+  require "core.options"
+else
+  local name = "nightly"
 
-require "core"
-require "plugin-loader"
+  require "core"
+  require "plugin-loader"
 
--- Check for theme configuration
--- Theme configs are can be found on lua/plugins/theme
-pcall(require, "plugins.theme." .. name)
+  -- Check for theme configuration
+  -- Theme configs are can be found on lua/plugins/theme
+  pcall(require, "plugins.theme." .. name)
 
--- Set the theme
-vim.cmd.colorscheme(name)
+  -- Set the theme
+  vim.cmd.colorscheme(name)
+end
