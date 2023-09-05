c629e0e66621cca19016e56801425dae942de82c
Alexis12119 <corporal461@gmail.com>
2023-09-05 18:12:22 +0800

    feat: use `telescope-fzf-native.nvim`.

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -254,6 +254,7 @@ local plugins = {
     "nvim-telescope/telescope.nvim",
     opts = {
       pickers = overrides.telescope_pickers,
+      extensions_list = { "fzf" },
     },
     dependencies = {
       {
