b85f714aef0f4c5a43c438a63ce43883313f42be
Alexis12119 <corporal461@gmail.com>
2023-09-07 22:29:45 +0800

    feat: add library in `lua_ls`.

--- lua/custom/configs/lsp/settings/lua_ls.lua
+++ lua/custom/configs/lsp/settings/lua_ls.lua
@@ -11,11 +11,12 @@ return {
       workspace = {
         library = {
           [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-          [vim.fn.stdpath "config" .. "/lua"] = true,
-          -- [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-          -- [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
-          -- [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
+          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+          [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
+          [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
         },
+        maxPreload = 100000,
+        preloadFileSize = 10000,
       },
     },
   },
