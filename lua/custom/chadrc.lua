c5aecda44c6506c9b109d7ece1557cda4ec4d8c4
Alexis12119 <corporal461@gmail.com>
2023-09-13 10:32:32 +0800

    feat: disable `tabufline` at startup.

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -77,7 +77,7 @@ M.ui = {
   },
 
   nvdash = {
-    load_on_startup = true,
+    load_on_startup = false,
     header = {
       "           ▄ ▄                   ",
       "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
