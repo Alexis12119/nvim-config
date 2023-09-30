8ad4f181e3429d632ac262fc071e4f9e789714bd
Alexis12119 <corporal461@gmail.com>
2023-10-01 04:44:02 +0800

    feat: use `curl` for installing parsers.

--- plugins.lua
+++ plugins.lua
@@ -206,6 +206,11 @@ local plugins = {
       "TSBufToggle",
     },
     opts = overrides.treesitter,
+    config = function(_, opts)
+      dofile(vim.g.base46_cache .. "syntax")
+      require("nvim-treesitter.install").prefer_git = false
+      require("nvim-treesitter.configs").setup(opts)
+    end,
     dependencies = {
       "JoosepAlviste/nvim-ts-context-commentstring",
       "nvim-treesitter/nvim-treesitter-textobjects",
