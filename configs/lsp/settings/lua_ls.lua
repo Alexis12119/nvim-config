40d4b62f0b523dfbdce871dbc31cf6289eb8ebfe
Alexis12119 <corporal461@gmail.com>
2023-11-09 05:36:20 +0800

    feat: disable `checkThirdParty`.

--- configs/lsp/settings/lua_ls.lua
+++ configs/lsp/settings/lua_ls.lua
@@ -16,6 +16,7 @@ return {
           [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
           [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
         },
+        checkThirdParty = false,
         maxPreload = 100000,
         preloadFileSize = 10000,
       },
