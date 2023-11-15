a373e7b4a8108f82f9500f9296ebe53deb883511
Alexis12119 <corporal461@gmail.com>
2023-11-15 17:27:25 +0800

    feat: update.

--- core/options.lua
+++ core/options.lua
@@ -36,7 +36,6 @@ local options = {
   termguicolors = true, -- Enables 24-bit RGB color in the TUI
   fillchars = { eob = " ", fold = " ", foldopen = "", foldsep = " ", foldclose = "", lastline = " " }, -- make EndOfBuffer invisible
   ruler = false,
-  autoread = false,
   -- shell = vim.fn.executable "pwsh" and "pwsh" or "powershell",
   -- shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
   -- shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
