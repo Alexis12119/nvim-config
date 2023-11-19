47b74c17f54e153cec118af80ee320df563b2b98
Alexis12119 <corporal461@gmail.com>
2023-11-19 16:48:00 +0800

    fix: doesn't take effect.

--- core/utilities.lua
+++ core/utilities.lua
@@ -152,16 +152,16 @@ function ClickUpdate()
       end
 
       -- Update to the latest NvChad commits
-      vim.fn.jobstart({ "git", "pull" }, { silent = true, cwd = nvchad_config })
+      vim.fn.system("git -C " .. nvchad_config .. " pull")
 
       -- Fetch the latest changes from the remote repository
-      vim.fn.jobstart({ "git", "fetch", "origin" }, { silent = true, cwd = config_path })
+      vim.fn.system("git -C " .. config_path .. " fetch origin")
 
-      -- Hard reset to the latest commit on the main config_branch
-      vim.fn.jobstart({ "git", "reset", "--hard", "origin", config_branch }, { silent = true, cwd = config_path })
+      -- Hard reset to the latest commit on the main branch
+      vim.fn.system("git -C " .. config_path .. " reset --hard origin/" .. config_branch)
 
       -- Pull the latest changes
-      vim.fn.jobstart({ "git", "pull", "origin", config_branch }, { silent = true, cwd = config_path })
+      vim.fn.system("git -C " .. config_path .. " pull origin " .. config_branch)
     end)
   end
 
