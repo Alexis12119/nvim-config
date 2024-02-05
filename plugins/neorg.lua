26ce2d34defb18194fb87996a5c8feb3ac71d3bd
Alexis12119 <corporal461@gmail.com>
2024-02-05 19:53:42 +0800

    feat: add `neorg`.

---
+++ plugins/neorg.lua
@@ -0,0 +1,29 @@
+return {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  ft = "norg",
+  config = function()
+    require("neorg").setup {
+      load = {
+        ["core.defaults"] = {},
+        ["core.keybinds"] = {
+          config = {
+            neorg_leader = ",",
+          },
+        },
+        ["core.concealer"] = {},
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              notes = "~/notes",
+            },
+            default_workspace = "notes",
+          },
+        },
+      },
+    }
+
+    vim.wo.foldlevel = 99
+    vim.wo.conceallevel = 2
+  end,
+}
