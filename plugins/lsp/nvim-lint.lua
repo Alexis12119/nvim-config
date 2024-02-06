1b1c94411d928f5bd30349b0278e2683cb307472
Alexis12119 <corporal461@gmail.com>
2024-02-06 10:15:21 +0800

    format: files.

--- plugins/lsp/nvim-lint.lua
+++ plugins/lsp/nvim-lint.lua
@@ -1,10 +1,9 @@
 require("lint").linters_by_ft = {
-          -- python = { "flake8" },
-        }
+  -- python = { "flake8" },
+}
 
-        vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-          callback = function()
-            require("lint").try_lint()
-          end,
-        })
-      
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  callback = function()
+    require("lint").try_lint()
+  end,
+})
