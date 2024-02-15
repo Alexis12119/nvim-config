bf3d1a327ccff9a2af967ca24fd0e28e310f50c9
Alexis12119 <corporal461@gmail.com>
2024-02-15 13:01:40 +0800

    feat: use `cinnamon` for smooth scrolling.

--- core/autocommands.lua
+++ core/autocommands.lua
@@ -304,14 +304,6 @@ autocmd("FileType", {
   end,
 })
 
-autocmd({ "FileType" }, {
-  pattern = { "help", "text", "log" },
-  callback = function()
-    vim.opt_local.scrolloff = 999
-  end,
-  desc = "Keep the cursor in the middle of the screen when scrolling",
-})
-
 -- For Godot
 local godot = augroup("Godot", { clear = true })
 
---
+++ plugins/cinnamon.lua
@@ -0,0 +1,12 @@
+return {
+  "declancm/cinnamon.nvim",
+  event = "User FilePost",
+  opts = {
+    -- KEYMAPS:
+    default_keymaps = true, -- Create default keymaps.
+    extra_keymaps = false, -- Create extra keymaps.
+    extended_keymaps = false, -- Create extended keymaps.
+    override_keymaps = false, -- The plugin keymaps will override any existing keymaps.
+    scroll_limit = -1,
+  },
+}
