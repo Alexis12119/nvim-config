bff02793c63c1e1413b705fc8ee318ba14d009d9
Alexis12119 <corporal461@gmail.com>
2024-03-14 11:17:14 +0800

    feat: cleanup

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -45,8 +45,6 @@ M.ui = {
     icons = true,
     lspkind_text = true,
     style = "default", -- default/flat_light/flat_dark/atom/atom_colored
-    border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
-    selected_item_bg = "colored", -- colored / simple
   },
   statusline = {
     theme = "minimal", -- default/vscode/vscode_colored/minimal
