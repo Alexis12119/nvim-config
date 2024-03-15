1d08716c74ba9025aad7bcd13788cca22302acfb
Alexis12119 <corporal461@gmail.com>
2024-03-15 22:08:40 +0800

    fix: the `ibl` config.

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,8 +3,7 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   enabled = false,
   event = "User FilePost",
-  opts = {
-    show_current_context = true,
-    show_current_context_start = false,
-  },
+  config = function()
+    require("ibl").setup()
+  end,
 }
