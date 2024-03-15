ef260825fca402d831793eba3d44da59aaf2bfdd
Alexis12119 <corporal461@gmail.com>
2024-03-15 22:10:45 +0800

    feat: enable `ibl`.

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,9 +1,12 @@
 -- NOTE: Show Indentlines
 return {
   "lukas-reineke/indent-blankline.nvim",
-  enabled = false,
   event = "User FilePost",
   config = function()
-    require("ibl").setup()
+    require("ibl").setup {
+      scope = {
+        show_start = false,
+      },
+    }
   end,
 }
