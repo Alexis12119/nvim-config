19a2d4536a520e8bdb9574d4e2182fd51b56183f
Alexis12119 <corporal461@gmail.com>
2024-05-09 08:45:56 +0800

    feat: enable other hl options

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -3,6 +3,19 @@ return {
   "NvChad/nvim-colorizer.lua",
   opts = {
     user_default_options = {
+      RGB = true, -- #RGB hex codes
+      RRGGBB = true, -- #RRGGBB hex codes
+      names = true, -- "Name" codes like Blue or blue
+      RRGGBBAA = true, -- #RRGGBBAA hex codes
+      AARRGGBB = true, -- 0xAARRGGBB hex codes
+      rgb_fn = true, -- CSS rgb() and rgba() functions
+      hsl_fn = true, -- CSS hsl() and hsla() functions
+      css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      -- Available modes for `mode`: foreground, background,  virtualtext
+      mode = "background", -- Set the display mode.
+      -- Available methods are false / true / "normal" / "lsp" / "both"
+      -- True is same as normal
       tailwind = true,
       sass = { enable = true, parsers = { "css" } }, -- Enable sass colors
       always_update = true,
