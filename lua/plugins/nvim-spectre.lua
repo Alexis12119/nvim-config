2a0f6e3146c45b2c7aea1a913578fa69ea00feaf
Alexis12119 <corporal461@gmail.com>
2024-05-15 19:36:29 +0800

    feat: `grug-far` instead of `nvim-spectre`.

--- lua/plugins/nvim-spectre.lua
+++ lua/plugins/nvim-spectre.lua
@@ -1,6 +1,7 @@
 -- NOTE: Find And Replace Words
 return {
   "nvim-pack/nvim-spectre",
+  enabled = false,
   init = function()
     vim.keymap.set("n", "<leader>fR", "<cmd>Spectre<cr>", { desc = "Spectre | Find And Replace", silent = true })
   end,
