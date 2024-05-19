7b95e2d0c904c0c3f22fbc910ee1513c194f2182
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-05-19 12:18:24 +0700

    fix(persisted): duplicate init func, event to trigger

--- lua/plugins/persisted.lua
+++ lua/plugins/persisted.lua
@@ -2,11 +2,20 @@
 return {
   "olimorris/persisted.nvim",
   enabled = true,
+  event = "VeryLazy",
   init = function()
     vim.keymap.set("n", "<leader>so", "<cmd>SessionStop<cr>", { desc = "Persisted | Stop", silent = true })
     vim.keymap.set("n", "<leader>sl", "<cmd>SessionLoad<cr>", { desc = "Persisted | Load", silent = true })
     vim.keymap.set("n", "<leader>sL", "<cmd>SessionLoad<cr>", { desc = "Persisted | Load Last", silent = true })
     vim.keymap.set("n", "<leader>sd", "<cmd>SessionDelete<cr>", { desc = "Persisted | Delete", silent = true })
+    -- close nvim-tree before exist while using persisted
+    vim.api.nvim_create_autocmd("ExitPre", {
+      callback = function()
+        require("nvim-tree.view").close_all_tabs()
+      end,
+      group = vim.api.nvim_create_augroup("Persisted", { clear = true }),
+      desc = "Close nvim-tree before exit",
+    })
   end,
   cmd = {
     "SessionSave",
@@ -24,14 +33,4 @@ return {
     autosave = true, -- automatically save session files when exiting Neovim
     autoload = false, -- automatically load the session for the cwd on Neovim startup
   },
-  init = function()
-    -- close nvim-tree before exist while using persisted
-    vim.api.nvim_create_autocmd("ExitPre", {
-      callback = function()
-        require("nvim-tree.view").close_all_tabs()
-      end,
-      group = vim.api.nvim_create_augroup("Persisted", { clear = true }),
-      desc = "Close nvim-tree before exit",
-    })
-  end,
 }
