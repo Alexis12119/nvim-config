bad8d18417a3024305f1a8030110b82c5b4ec4dd
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-05-25 00:21:23 +0700

    fix(wakatime): typo `enabled`

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -2,6 +2,6 @@ return {
   {
     "wakatime/vim-wakatime",
     event = "VeryLazy",
-    enable = vim.fn.executable "wakatime" and vim.fn.filereadable "~/.wakatime.cfg",
+    enabled = vim.fn.executable "wakatime" and vim.fn.filereadable "~/.wakatime.cfg",
   },
 }
