3d35af2b05d8480a93037b3c2c5f26b6a940fa9f
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-05-25 08:41:24 +0700

    fix(wakatime): only need to check wakatime config

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -2,7 +2,6 @@ return {
   {
     "wakatime/vim-wakatime",
     event = "VeryLazy",
-    enabled = (vim.fn.executable "wakatime" or vim.fn.executable "wakatime-cli")
-      and vim.fn.filereadable(vim.fn.getenv "HOME" .. "/.wakatime.cfg"),
+    enabled = vim.fn.filereadable(vim.fn.getenv "HOME" .. "/.wakatime.cfg"),
   },
 }
