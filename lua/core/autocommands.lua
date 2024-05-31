de3d9e112801e97a712b907a597d403f1c69750c
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-05-31 09:32:03 +0700

    chore(`copilot.vim`): remove `fg` color

--- lua/core/autocommands.lua
+++ lua/core/autocommands.lua
@@ -229,7 +229,6 @@ autocmd("ColorScheme", {
   pattern = "solarized",
   callback = function()
     vim.api.nvim_set_hl(0, "CopilotSuggestion", {
-      fg = "#555555",
       ctermfg = 8,
       force = true,
     })
