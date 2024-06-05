f6ed08546adfd09b065e0cf56e99d46c87e451f8
Alexis12119 <corporal461@gmail.com>
2024-06-05 23:40:43 +0800

    feat: disable `typescript-tools.nvim` formatting.

--- lua/plugins/lsp/configs/typescript-tools.lua
+++ lua/plugins/lsp/configs/typescript-tools.lua
@@ -3,6 +3,10 @@ return {
   "pmizio/typescript-tools.nvim",
   event = "User FilePost",
   opts = {
+    on_attach = function(client)
+      client.server_capabilities.documentFormattingProvider = false
+      client.server_capabilities.documentRangeFormattingProvider = false
+    end,
     settings = {
       tsserver_file_preferences = {
         includeInlayParameterNameHints = "all",
