753032613d3476008721dfbeb0ea851b2372d347
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-06-09 19:47:57 +0700

    feat(python venv): migrate to `venv-selector`

---
+++ lua/plugins/venv-selector.lua
@@ -0,0 +1,13 @@
+-- NOTE: For switching between Python venv
+return {
+  "linux-cultist/venv-selector.nvim",
+  branch = "regexp",
+  cmd = { "VenvSelect", "VenvSelectCached" },
+  init = function()
+    vim.keymap.set("n", "<leader>vs", "<cmd>VenvSelect<cr>", { desc = "Venv Selector | Select Python venv" })
+  end,
+  opts = {},
+  config = function(_, opts)
+    require("venv-selector").setup(opts)
+  end,
+}
