5c9d76802f8ab648232cd083987af6e35b687322
Alexis12119 <corporal461@gmail.com>
2024-06-12 21:27:05 +0800

    fix: luasnip warning.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -56,13 +56,13 @@ return {
         ["cmp.entry.get_documentation"] = true,
       },
       hover = {
-        enabled = false,
+        enabled = true,
         silent = false, -- set to true to not show a message if hover is not available
         view = nil, -- when nil, use defaults from documentation
         opts = {}, -- merged with defaults from documentation
       },
       signature = {
-        enabled = false,
+        enabled = true,
         auto_open = {
           enabled = true,
           trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -54,6 +54,7 @@ return {
     {
       "L3MON4D3/LuaSnip",
       dependencies = "rafamadriz/friendly-snippets",
+      build = "make install_jsregexp",
     },
   },
 }
