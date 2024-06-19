d82f6583ef691fe3aac145666eb82e9a8110aac9
Alexis12119 <corporal461@gmail.com>
2024-06-19 19:15:47 +0800

    feat: enable `tsserver` on `vue`.

--- lua/plugins/lsp/settings/volar.lua
+++ lua/plugins/lsp/settings/volar.lua
@@ -1,6 +1,10 @@
 return {
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
   settings = {
     init_options = {
+      vue = {
+        hybridMode = false,
+      },
       typescript = {
         tsdk = vim.fn.stdpath "data" .. "/mason/packages/vue-language-server/node_modules/typescript/lib",
       },
