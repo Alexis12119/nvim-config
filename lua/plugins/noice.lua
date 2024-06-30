34ee81c01abc348a4b8ebee6c9157a3110291a98
Alexis12119 <corporal461@gmail.com>
2024-06-30 21:35:22 +0800

    feat: change keymap for `nvim-treesitter` selection mode.

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -56,13 +56,13 @@ return {
         ["cmp.entry.get_documentation"] = true,
       },
       hover = {
-        enabled = true,
+        enabled = false,
         silent = false, -- set to true to not show a message if hover is not available
         view = nil, -- when nil, use defaults from documentation
         opts = {}, -- merged with defaults from documentation
       },
       signature = {
-        enabled = true,
+        enabled = false,
         auto_open = {
           enabled = true,
           trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -162,7 +162,7 @@ return {
         selection_modes = {
           ["@parameter.outer"] = "v", -- charwise
           ["@function.outer"] = "V", -- linewise
-          ["@class.outer"] = "<c-v>", -- blockwise
+          ["@class.outer"] = "V", -- linewise
         },
         -- If you set this to `true` (default is `false`) then any textobject is
         -- extended to include preceding or succeeding whitespace. Succeeding
