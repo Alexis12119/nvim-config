ff7a9cf18dbf421f8154e2fd9691c4b119c6debd
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-06-30 16:50:22 +0700

    chore(cinnamon): format the config

--- lua/plugins/cinnamon.lua
+++ lua/plugins/cinnamon.lua
@@ -4,15 +4,15 @@ return {
   event = "User FilePost",
   opts = {
     keymaps = {
-        basic = true,           -- Enable the basic keymaps
-        extra = false,          -- Enable the extra keymaps
+      basic = true, -- Enable the basic keymaps
+      extra = false, -- Enable the extra keymaps
     },
     options = {
-        delay = 7,              -- Delay between each movement step (in ms)
-        max_delta = {
-            line = 150,         -- Maximum delta for line movements
-            column = 200,       -- Maximum delta for column movements
-        },
+      delay = 7, -- Delay between each movement step (in ms)
+      max_delta = {
+        line = 150, -- Maximum delta for line movements
+        column = 200, -- Maximum delta for column movements
+      },
     },
   },
 }
