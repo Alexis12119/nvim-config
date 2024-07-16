627b3b8f65618612134f16db434a40bae08c57c4
Alexis12119 <corporal461@gmail.com>
2024-07-16 10:11:18 +0800

    feat(vue): also add through `vtsls`.

---
+++ lua/plugins/lsp/settings/vtsls.lua
@@ -0,0 +1,26 @@
+return {
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+    "vue",
+  },
+  settings = {
+    vtsls = {
+      tsserver = {
+        globalPlugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = vim.fn.stdpath "data" .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+            languages = { "vue" },
+            configNamespace = "typescript",
+            enableForWorkspaceTypeScriptVersions = true,
+          },
+        },
+      },
+    },
+  },
+}
