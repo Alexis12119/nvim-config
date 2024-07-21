0881704ba282a6fea5badd33a6b85f05540fa549
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-07-21 21:43:52 +0700

    fix(cloak): match right patterns
    also add new keymap `CloakPreviewLine`

--- lua/plugins/cloak.lua
+++ lua/plugins/cloak.lua
@@ -2,20 +2,29 @@ return {
   {
     "laytan/cloak.nvim",
     init = function()
-      vim.keymap.set("n", "<leader>oC", "<cmd>CloakToggle<cr>", { desc = "Cloak | Toggle Cloak", silent = true })
+      vim.keymap.set("n", "<leader>oH", "<cmd>CloakToggle<cr>", { desc = "Cloak | Toggle Cloak", silent = true })
+      vim.keymap.set("n", "<leader>oh", "<cmd>CloakPreviewLine<cr>", { desc = "Cloak | Preview Line", silent = true })
     end,
-    ft = { "yaml", "json", "jsonc", "toml", "env" },
-    cmd = { "CloakToggle", "CloakEnable", "CloakDisable" },
+    ft = {
+      "yaml",
+      "json",
+      "jsonc",
+      "toml",
+      "env",
+    },
+    cmd = {
+      "CloakToggle",
+      "CloakEnable",
+      "CloakDisable",
+      "CloakPreviewLine",
+    },
     opts = {
       cloak_length = "5",
       patterns = {
         {
           file_pattern = {
-            "**/*env*.{json,jsonc,yml,yaml,toml,env}",
-            "**/*account*.{json,jsonc,yml,yaml,toml,env}",
-            "**/*credential*.{json,jsonc,yml,yaml,toml,env}",
-            "**/*password*.{json,jsonc,yml,yaml,toml,env}",
-            "**/*config*.{json,jsonc,yml,yaml,toml,env}",
+            ".env*",
+            "*{account,credential,password,config}*.{json,jsonc,yml,yaml,toml,env}",
           },
           cloak_pattern = {
             "=.+",
