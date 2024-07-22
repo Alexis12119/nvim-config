4c0fc7a05ad47fdf7ea85519ef5ae0f0eee547f9
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-07-22 13:41:10 +0700

    feat(plugins): add `barrett-ruth/live-server`

---
+++ lua/plugins/live-server.lua
@@ -0,0 +1,26 @@
+return {
+
+  "barrett-ruth/live-server.nvim",
+  build = "npm add -g live-server",
+  cmd = {
+    "LiveServerStart",
+    "LiveServerStop",
+  },
+  init = function()
+    local toggle_state = (function()
+      local state = true
+      return function()
+        state = not state
+        return state
+      end
+    end)()
+    vim.keymap.set("n", "<leader>ul", function()
+      if toggle_state() then
+        vim.cmd "LiveServerStart"
+      else
+        vim.cmd "LiveServerStop"
+      end
+    end, { desc = "LiveServer | Toggle", silent = true })
+  end,
+  config = true,
+}
