8c3b0fc2ab50d67d56cd7c870510f585e72df084
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-07-24 18:14:53 +0700

    chore(wakatime): change variable name

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -2,12 +2,12 @@
 -- Type ":Lazy load vim-wakatime" to generate ".wakatime.cfg".
 -- You only need to do this once.
 -- To disable, remove "~./wakatime.cfg".
-local wakatime = {
+local plugin = {
   "wakatime/vim-wakatime",
 }
 
 if vim.fn.filereadable(vim.fn.getenv "HOME" .. "/.wakatime.cfg") == 1 then
-  wakatime.event = "User FilePost"
+  plugin.event = "User FilePost"
 end
 
-return wakatime
+return plugin
