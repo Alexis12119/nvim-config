8bc8740be8689d44c38fc0ad9a23f74870ccf3d2
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-08-02 17:25:05 +0700

    chore(nvim-cmp): local functions inside config function

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,75 +1,5 @@
 ---@type NvPluginSpec
 -- NOTE: Completion Engine
-
---NOTE: Supertab setup referenced from LazyVim documentation
--- Supertab from LazyVim: https://www.lazyvim.org/configuration/recipes#supertab
--- Copilot tab completion: https://github.com/zbirenbaum/copilot-cmp?tab=readme-ov-file#tab-completion-configuration-highly-recommended
--- Neogen tab cycling: https://github.com/danymat/neogen?tab=readme-ov-file#default-cycling-support
-
-local setup_supertab_forward = function()
-  local cmp = require "cmp"
-
-  local has_words_before = function()
-    unpack = unpack or table.unpack
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-  end
-
-  return cmp.mapping(function(fallback)
-    if cmp.visible() then
-      -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-      cmp.select_next_item()
-    elseif vim.snippet.active { direction = 1 } then
-      vim.schedule(function()
-        vim.snippet.jump(1)
-      end)
-    elseif has_words_before() then
-      cmp.complete()
-    else
-      fallback()
-    end
-  end, { "i", "s" })
-end
-
-local setup_supertab_backward = function()
-  local cmp = require "cmp"
-  return cmp.mapping(function(fallback)
-    if cmp.visible() then
-      cmp.select_prev_item()
-      -- elseif neogen.jumpable(true) then
-      --   neogen.jump_prev()
-    elseif vim.snippet.active { direction = -1 } then
-      vim.schedule(function()
-        vim.snippet.jump(-1)
-      end)
-    else
-      fallback()
-    end
-  end, { "i", "s" })
-end
-
--- NOTE: Ref: https://github.com/hrsh7th/nvim-cmp/issues/429#issuecomment-954121524
-local setup_toggle_autocomplete_menu = function()
-  local cmp = require "cmp"
-  return cmp.mapping {
-    i = function()
-      if cmp.visible() then
-        cmp.abort()
-      else
-        cmp.complete()
-      end
-    end,
-    c = function()
-      if cmp.visible() then
-        cmp.close()
-      else
-        cmp.complete()
-      end
-    end,
-  }
-end
-
----@type NvPluginSpec
 return {
   "hrsh7th/nvim-cmp",
   event = { "InsertEnter", "CmdlineEnter" },
@@ -84,6 +14,72 @@ return {
     end, { desc = "Options | Toggle Autocomplete" })
   end,
   config = function(_, opts)
+    --NOTE: Supertab setup referenced from LazyVim documentation
+    -- Supertab from LazyVim: https://www.lazyvim.org/configuration/recipes#supertab
+    -- Copilot tab completion: https://github.com/zbirenbaum/copilot-cmp?tab=readme-ov-file#tab-completion-configuration-highly-recommended
+    -- Neogen tab cycling: https://github.com/danymat/neogen?tab=readme-ov-file#default-cycling-support
+
+    local setup_supertab_forward = function()
+      local cmp = require "cmp"
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+      end
+
+      return cmp.mapping(function(fallback)
+        if cmp.visible() then
+          -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+          cmp.select_next_item()
+        elseif vim.snippet.active { direction = 1 } then
+          vim.schedule(function()
+            vim.snippet.jump(1)
+          end)
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+    end
+
+    local setup_supertab_backward = function()
+      local cmp = require "cmp"
+      return cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif vim.snippet.active { direction = -1 } then
+          vim.schedule(function()
+            vim.snippet.jump(-1)
+          end)
+        else
+          fallback()
+        end
+      end, { "i", "s" })
+    end
+
+    -- NOTE: Ref: https://github.com/hrsh7th/nvim-cmp/issues/429#issuecomment-954121524
+    local setup_toggle_autocomplete_menu = function()
+      local cmp = require "cmp"
+      return cmp.mapping {
+        i = function()
+          if cmp.visible() then
+            cmp.abort()
+          else
+            cmp.complete()
+          end
+        end,
+        c = function()
+          if cmp.visible() then
+            cmp.close()
+          else
+            cmp.complete()
+          end
+        end,
+      }
+    end
+
     dofile(vim.g.base46_cache .. "cmp")
 
     table.insert(opts.sources, 2, { name = "copilot" })
