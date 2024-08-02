93de215813bf5690b592bdfc910816dee683fe94
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-08-02 21:18:28 +0700

    chore(nvim-cmp): no enter default suggestion on cmd (Alexis)

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -136,12 +136,12 @@ return {
 
     require("cmp").setup(opts)
 
-    local cmdline_mappings = vim.tbl_extend("force", {}, require("cmp").mapping.preset.cmdline(), {
-      ["<CR>"] = { c = require("cmp").mapping.confirm { select = true } },
-    })
+    -- local cmdline_mappings = vim.tbl_extend("force", {}, require("cmp").mapping.preset.cmdline(), {
+    --   ["<CR>"] = { c = require("cmp").mapping.confirm { select = true } },
+    -- })
 
     require("cmp").setup.cmdline(":", {
-      mapping = cmdline_mappings,
+      -- mapping = cmdline_mappings,
       sources = {
         { name = "cmdline" },
       },
