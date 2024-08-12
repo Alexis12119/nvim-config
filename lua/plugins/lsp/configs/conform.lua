d796ab016e9eee5d1e5d7dd3a4d56970f2ae8540
Alexis12119 <corporal461@gmail.com>
2024-08-13 07:15:41 +0800

    fix(conform): other ft that supports `prettier` can't be overriden.

--- lua/plugins/lsp/configs/conform.lua
+++ lua/plugins/lsp/configs/conform.lua
@@ -6,27 +6,20 @@ local formatters_by_ft = {
   go = { "gofumpt" },
   cs = { "csharpier" },
   yaml = { "yamlfmt" },
+  css = { "prettier" },
+  flow = { "prettier" },
+  graphql = { "prettier" },
+  html = { "prettier" },
+  json = { "prettier" },
+  javascriptreact = { "prettier" },
+  javascript = { "prettier" },
+  less = { "prettier" },
+  markdown = { "prettier" },
+  scss = { "prettier" },
+  typescript = { "prettier", "deno_fmt" },
+  typescriptreact = { "prettier" },
+  vue = { "prettier" },
 }
-
-local prettier_ft = {
-  "css",
-  "flow",
-  "graphql",
-  "html",
-  "json",
-  "javascriptreact",
-  "javascript",
-  "less",
-  "markdown",
-  "scss",
-  "typescript",
-  "typescriptreact",
-  "vue",
-}
-
-for _, filetype in pairs(prettier_ft) do
-  formatters_by_ft[filetype] = { "prettier" }
-end
 
 ---@type NvPluginSpec
 return {
