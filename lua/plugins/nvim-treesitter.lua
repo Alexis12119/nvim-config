b27826fc18fdb009b523903378e1e0df4949b0d4
Alexis12119 <corporal461@gmail.com>
2024-08-14 11:53:08 +0800

    revert(auto-tag):I thought I was wrong :).

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -24,10 +24,12 @@ return {
     {
       "windwp/nvim-ts-autotag",
       opts = {
-        -- Defaults
-        enable_close = true, -- Auto close tags
-        enable_rename = true, -- Auto rename pairs of tags
-        enable_close_on_slash = false, -- Auto close on trailing </
+        opts = {
+          -- Defaults
+          enable_close = true, -- Auto close tags
+          enable_rename = true, -- Auto rename pairs of tags
+          enable_close_on_slash = false, -- Auto close on trailing </
+        },
       },
     },
     "nvim-treesitter/nvim-treesitter-textobjects",
