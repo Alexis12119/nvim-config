0be3c2a54b2dcfc41d81a311ec7ea6f13f0c3052
Alexis12119 <corporal461@gmail.com>
2024-08-14 09:42:55 +0800

    fix(css): unknown rules `@tailwind`.

---
+++ lua/plugins/lsp/settings/cssls.lua
@@ -0,0 +1,19 @@
+return {
+  settings = {
+    css = {
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+    scss = {
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+    less = {
+      lint = {
+        unknownAtRules = "ignore",
+      },
+    },
+  },
+}
