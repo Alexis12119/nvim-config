77fe33b260ac6845697ad63ab77cef88b2dc8d68
Alexis12119 <corporal461@gmail.com>
2024-08-14 11:09:39 +0800

    fix(nvim-ts-autotag): nested `opts`.

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -24,12 +24,10 @@ return {
     {
       "windwp/nvim-ts-autotag",
       opts = {
-        opts = {
-          -- Defaults
-          enable_close = true, -- Auto close tags
-          enable_rename = true, -- Auto rename pairs of tags
-          enable_close_on_slash = false, -- Auto close on trailing </
-        },
+        -- Defaults
+        enable_close = true, -- Auto close tags
+        enable_rename = true, -- Auto rename pairs of tags
+        enable_close_on_slash = false, -- Auto close on trailing </
       },
     },
     "nvim-treesitter/nvim-treesitter-textobjects",
