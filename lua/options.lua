c5b52fe65c68174480ccb6540bf7149bb9f2b592
Alexis12119 <corporal461@gmail.com>
2024-08-22 15:28:55 +0800

    feat(options): improve.

--- lua/options.lua
+++ lua/options.lua
@@ -74,4 +74,6 @@ end
 
 -- add binaries installed by mason.nvim to path
 local is_windows = vim.fn.has "win32" ~= 0
-vim.env.PATH = vim.fn.stdpath "data" .. "/mason/bin" .. (is_windows and ";" or ":") .. vim.env.PATH
+local sep = is_windows and "\\" or "/"
+local delim = is_windows and ";" or ":"
+vim.env.PATH = table.concat({ vim.fn.stdpath "data", "mason", "bin" }, sep) .. delim .. vim.env.PATH
