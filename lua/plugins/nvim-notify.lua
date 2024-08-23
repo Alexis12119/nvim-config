0a4a9a0820b254640c0427f4595850f0dcacba0d
Alexis12119 <corporal461@gmail.com>
2024-08-23 20:40:28 +0800

    fix(nvim-notify): handle when `transparency` is on by default.

--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -13,6 +13,12 @@ return {
     top_down = false,
   },
   config = function(_, opts)
+    local base46 = require("nvconfig").base46
+
+    if base46.transparency then
+      opts.background_colour = "#000000"
+    end
+
     require("notify").setup(opts)
 
     vim.notify = require "notify"
