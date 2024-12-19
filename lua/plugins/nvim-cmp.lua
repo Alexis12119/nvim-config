1ece65025e9712808bb750bba645e15ccaa740ea
Alexis12119 <corporal461@gmail.com>
2024-12-20 07:27:01 +0800

    chore: separate `supermaven` from `nvim-cmp`.

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -76,21 +76,6 @@ return {
       },
     },
     {
-      "supermaven-inc/supermaven-nvim",
-      -- commit = "df3ecf7",
-      -- commit = "40bde487fe31723cdd180843b182f70c6a991226",
-      event = "User FilePost",
-      opts = {
-        disable_keymaps = false,
-        disable_inline_completion = false,
-        keymaps = {
-          accept_suggestion = "<A-f>",
-          clear_suggestion = "<Nop>",
-          accept_word = "<A-w>",
-        },
-      },
-    },
-    {
       "L3MON4D3/LuaSnip",
       dependencies = "rafamadriz/friendly-snippets",
       build = "make install_jsregexp",
