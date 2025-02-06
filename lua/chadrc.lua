ae4ded10574bcbb409ab8be7b6029c557fd46246
Alexis12119 <corporal461@gmail.com>
2025-02-06 09:37:40 +0800

    feat(theme): change it.

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -137,9 +137,9 @@ M.mason = {
 M.lsp = { signature = false }
 
 M.base46 = {
-  theme = "mountain",
+  theme = "rxyhn",
   transparency = false,
-  theme_toggle = { "mountain", "one_light" },
+  theme_toggle = { "rxyhn", "one_light" },
   hl_override = highlights.override,
   hl_add = highlights.add,
   integrations = {
