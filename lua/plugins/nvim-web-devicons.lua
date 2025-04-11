fc8c5d226893d371b2ee28ca1e61ccb00b249868
Alexis12119 <corporal461@gmail.com>
2025-04-11 20:55:31 +0800

    fix(icons): config issue.

--- lua/plugins/nvim-web-devicons.lua
+++ lua/plugins/nvim-web-devicons.lua
@@ -1,13 +1,13 @@
 ---@type NvPluginSpec
 -- NOTE: Icons
 
--- return {
---   "nvim-tree/nvim-web-devicons",
---   config = function()
---     local icons = require "nvim-web-devicons"
---
---     icons.set_icon {
---       deb = { icon = "", name = "Deb", color = "#A1B7EE" },
---     }
---   end,
--- }
+return {
+  "nvim-tree/nvim-web-devicons",
+  config = function()
+    local icons = require "nvim-web-devicons"
+
+    icons.set_icon {
+      deb = { icon = "", name = "Deb", color = "#A1B7EE" },
+    }
+  end,
+}
