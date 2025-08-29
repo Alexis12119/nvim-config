8849364096ca963e4ff70d73fa5306cf374c727d
Alexis12119 <corporal461@gmail.com>
2025-08-29 18:41:10 +0800

    feat(theme): change to `rxyhn`.

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -137,9 +137,9 @@ M.mason = {
 M.lsp = { signature = false }
 
 M.base46 = {
-  theme = "kanagawa",
+  theme = "rxyhn",
   transparency = false,
-  theme_toggle = { "kanagawa", "one_light" },
+  theme_toggle = { "rxyhn", "one_light" },
   hl_override = highlights.override,
   hl_add = highlights.add,
   integrations = {
