351fd7efc32bbe53bafc8dc4e706406388ec1720
Alexis12119 <corporal461@gmail.com>
2025-09-02 22:51:08 +0800

    feat(plugin): add `image.nvim`.
    It's not enabled by default for windows.

---
+++ lua/plugins/image.lua
@@ -0,0 +1,46 @@
+---@type NvPluginSpec
+-- NOTE: Image Support
+return {
+  "3rd/image.nvim",
+  event = "VeryLazy",
+  enabled = vim.g.is_windows,
+  opts = {
+    backend = "kitty",
+    processor = "magick_cli",
+    integrations = {
+      markdown = {
+        enabled = true,
+        clear_in_insert_mode = false,
+        download_remote_images = true,
+        only_render_image_at_cursor = true,
+        only_render_image_at_cursor_mode = "popup", -- or "inline"
+        floating_windows = false, -- if true, images will be rendered in floating markdown windows
+        filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+      },
+      neorg = {
+        enabled = true,
+        filetypes = { "norg" },
+      },
+      typst = {
+        enabled = true,
+        filetypes = { "typst" },
+      },
+      html = {
+        enabled = false,
+      },
+      css = {
+        enabled = false,
+      },
+    },
+    max_width = nil,
+    max_height = nil,
+    max_width_window_percentage = nil,
+    max_height_window_percentage = 50,
+    scale_factor = 1.0,
+    window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+    window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "snacks_notif", "scrollview", "scrollview_sign" },
+    editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+    tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
+  },
+}
