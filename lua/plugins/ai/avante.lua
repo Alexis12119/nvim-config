08d0f65f9fe5de0a536081fa5a8386195b0df32f
Alexis12119 <corporal461@gmail.com>
2025-09-07 23:05:32 +0800

    fix(avante): handle windows installation.

--- lua/plugins/ai/avante.lua
+++ lua/plugins/ai/avante.lua
@@ -3,10 +3,15 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
+  build = vim.fn.has "win32" ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+    or "make",
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   ---@module 'avante'
   ---@type avante.Config
   opts = {
+    behaviour = {
+      enable_token_counting = false,
+    },
     -- add any opts here
     -- for example
     provider = "gemini",
@@ -19,9 +24,6 @@ return {
       },
     },
   },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
