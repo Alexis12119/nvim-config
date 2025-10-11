d6ffb42e0a34a95a071eba97b2ae0a07d35b4046
Alexis12119 <corporal461@gmail.com>
2025-10-12 05:31:35 +0800

    feat(auto-session): use the `cmd` that works.

--- lua/plugins/sessions/auto-session.lua
+++ lua/plugins/sessions/auto-session.lua
@@ -8,7 +8,7 @@ return {
   },
   init = function()
     vim.keymap.set("n", "<leader>st", "<cmd>SessionToggleAutoSave<cr>", { desc = "Session | Toggle", silent = true })
-    vim.keymap.set("n", "<leader>sS", "<cmd>SessionSearch<cr>", { desc = "Session | Search", silent = true })
+    vim.keymap.set("n", "<leader>sS", "<cmd>Telescope session-lens<cr>", { desc = "Session | Search", silent = true })
     vim.keymap.set("n", "<leader>sd", "<cmd>SessionDelete<cr>", { desc = "Session | Delete", silent = true })
     vim.keymap.set("n", "<leader>sr", "<cmd>SessionRestore<cr>", { desc = "Session | Restore", silent = true })
     vim.keymap.set("n", "<leader>ss", "<cmd>SessionSave<cr>", { desc = "Session | Save", silent = true })
@@ -36,6 +36,10 @@ return {
     bypass_save_filetypes = {
       "NvimTree",
       "nvdash",
+    },
+    session_lens = {
+      picker = "telescope",
+      load_on_setup = true,
     },
   },
 }
