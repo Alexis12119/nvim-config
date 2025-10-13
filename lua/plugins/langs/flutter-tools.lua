6270a78e43a6377058585ddcd9ecbcc546346f56
Alexis12119 <corporal461@gmail.com>
2025-10-13 19:34:29 +0800

    feat: update.
    - add dart support for the `github_dark_dimmed`.
    - add `on_init` in `typescript-tools.nvim` and `flutter-tools.nvim` to
    disable semantic highlights.

--- lua/plugins/langs/flutter-tools.lua
+++ lua/plugins/langs/flutter-tools.lua
@@ -11,6 +11,7 @@ return {
     lsp = {
       on_attach = function(client, bufnr)
         require("plugins.lsp.opts").on_attach(client, bufnr)
+        require("plugins.lsp.opts").on_init(client, bufnr)
       end,
       capabilities = function()
         return require("plugins.lsp.opts").capabilities
--- lua/plugins/lsp/configs/typescript-tools.lua
+++ lua/plugins/lsp/configs/typescript-tools.lua
@@ -11,6 +11,7 @@ return {
       client.server_capabilities.documentFormattingProvider = false
       client.server_capabilities.documentRangeFormattingProvider = false
       require("plugins.lsp.opts").on_attach(client, bufnr)
+      require("plugins.lsp.opts").on_init(client, bufnr)
     end,
     settings = {
       separate_diagnostic_server = true, -- Disable separate diagnostic server
