7116805d37d12279d7d48d13accf494a59be78ed
Alexis12119 <corporal461@gmail.com>
2025-10-21 10:02:36 +0800

    feat(flash): move to `navigation` folder.

--- lua/plugins/terminal/toggleterm.lua
+++ lua/plugins/terminal/toggleterm.lua
@@ -93,7 +93,7 @@ return {
     },
     float_opts = {
       border = "rounded",
-      height = math.ceil(vim.o.lines * 1.0 - 6),
+      height = math.ceil(vim.o.lines * 1.0 - 4),
       width = math.ceil(vim.o.columns * 0.8),
       winblend = 0,
     },
