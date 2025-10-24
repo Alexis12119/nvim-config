02ca33465a80ba39ec88c9abd3d7381e8b0b41e9
Alexis12119 <corporal461@gmail.com>
2025-10-25 07:41:22 +0800

    fieat: jinja support

--- lua/core/filetypes.lua
+++ lua/core/filetypes.lua
@@ -2,6 +2,7 @@
 vim.filetype.add {
   extension = {
     ["axaml"] = "xml",
+    ["jinja"] = "htmldjango",
   },
   pattern = {
     [".*/%.github[%w/]+workflows[%w/]+.*%.ya?ml"] = "yaml.github",
---
+++ lua/plugins/lsp/settings/jinja_lsp.lua
@@ -0,0 +1,3 @@
+return {
+  filetypes = { "jinja", "htmldjango" },
+}
