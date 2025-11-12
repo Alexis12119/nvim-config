1cdc42a364f9f3e285d6f931167998076b6b2eea
Alexis12119 <corporal461@gmail.com>
2025-11-12 23:54:52 +0800

    feat: move  the base_46 global var.

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
-
 require("config.lazy")
 require("config.env")
 require("config.commands")
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,3 +1,5 @@
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
