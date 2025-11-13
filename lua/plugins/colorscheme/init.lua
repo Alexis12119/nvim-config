89102e0833516858c9435d5ac1c5b5a1f56694fe
Alexis12119 <corporal461@gmail.com>
2025-11-13 09:23:04 +0800

    fix: remove `ReloadNvChad`.

--- lua/plugins/colorscheme/init.lua
+++ lua/plugins/colorscheme/init.lua
@@ -11,13 +11,21 @@ return {
   {
     "LazyVim/LazyVim",
     opts = function(_, opts)
-      opts.colorscheme = "tokyonight"
+      opts.colorscheme = "nvchad"
       vim.g.colorscheme = opts.colorscheme
 
       if opts.colorscheme == "nvchad" then
         for _, v in ipairs(vim.fn.readdir(vim.g.base46_cache)) do
           dofile(vim.g.base46_cache .. v)
         end
+      else
+        -- Disable NvChad auto-reload on save
+        vim.api.nvim_create_autocmd("User", {
+          pattern = "VeryLazy", -- triggers once everything is loaded
+          callback = function()
+            pcall(vim.api.nvim_del_augroup_by_name, "ReloadNvChad")
+          end,
+        })
       end
     end,
   },
