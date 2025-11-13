9e734a622187b45cd0881a9ddfb3a37e249037fa
Alexis12119 <corporal461@gmail.com>
2025-11-13 23:43:11 +0800

    feat: make the snacks path clear.

--- lua/plugins/colorscheme/tokyonight.lua
+++ lua/plugins/colorscheme/tokyonight.lua
@@ -4,12 +4,13 @@ return {
   "folke/tokyonight.nvim",
   opts = {
     style = "night",
-    on_highlights = function(hl, _)
+    on_highlights = function(hl, c)
       hl.DiffviewDiffDelete = { bg = "#422b2b", fg = "#422b2b" }
 
       hl.LspReferenceRead = { link = "Underlined" }
       hl.LspReferenceText = { link = "Underlined" }
       hl.LspReferenceWrite = { link = "Underlined" }
+      hl.SnacksPickerDir = { fg = c.fg }
     end,
   },
 }
