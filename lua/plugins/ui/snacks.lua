46298f6b2d2e21741a4fdca37d2a07ae5b896320
Alexis12119 <corporal461@gmail.com>
2025-11-13 23:50:41 +0800

    feat: use buffer instead of `picker` for notifs.

--- lua/plugins/ui/snacks.lua
+++ lua/plugins/ui/snacks.lua
@@ -4,4 +4,13 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  keys = {
+    {
+      "<leader>n",
+      function()
+        Snacks.notifier.show_history()
+      end,
+      desc = "Notification History",
+    },
+  },
 }
