225f3daf9e8f9ffe2c8ce38a1a41a0c9dbfbc9d4
Alexis12119 <corporal461@gmail.com>
2025-11-15 21:58:23 +0800

    feat: update.

--- init.lua
+++ init.lua
@@ -3,3 +3,9 @@ require("config.env")
 require("config.commands")
 require("config.filetypes")
 require("config.utils")
+
+if vim.g.colorscheme == "nvchad" then
+  -- load theme
+  dofile(vim.g.base46_cache .. "defaults")
+  dofile(vim.g.base46_cache .. "statusline")
+end
