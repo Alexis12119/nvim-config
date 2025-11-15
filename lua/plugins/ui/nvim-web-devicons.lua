809d4482aad44e5277b8c747f508e6ae8e0ae0c9
Alexis12119 <corporal461@gmail.com>
2025-11-15 22:06:30 +0800

    chore: cleanup.

--- lua/plugins/ui/nvim-web-devicons.lua
+++ lua/plugins/ui/nvim-web-devicons.lua
@@ -1,9 +1,6 @@
 return {
   {
     "nvim-tree/nvim-web-devicons",
-    opts = function()
-      dofile(vim.g.base46_cache .. "devicons")
-    end,
   },
   { "nvim-mini/mini.icons", version = "*", enabled = false },
 }
