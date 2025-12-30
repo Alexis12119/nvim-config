47d133b4be2319d98f81f099d11340734ab0378d
Alexis12119 <corporal461@gmail.com>
2025-12-30 21:31:02 +0800

    feat: add safety checks for the workspace diagnostic plugin.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -217,7 +217,40 @@ autocmd("LspAttach", {
     end
 
     -- Populate workspace diagnostics (external plugin)
-    require("workspace-diagnostics").populate_workspace_diagnostics(client, vim.api.nvim_get_current_buf())
+    local ok, workspace_diagnostics = pcall(require, "workspace-diagnostics")
+    if not ok then
+      return
+    end
+
+    -- NOTE: Populate workspace diagnostics
+    workspace_diagnostics.populate_workspace_diagnostics(client, vim.api.nvim_get_current_buf())
   end,
   desc = "Customize LSP behavior and populate workspace diagnostics",
+})
+
+autocmd("FileType", {
+  pattern = "qf",
+  callback = function()
+    vim.keymap.set("n", "dd", function()
+      local qf_list = vim.fn.getqflist()
+
+      local current_line_number = vim.fn.line(".")
+
+      if qf_list[current_line_number] then
+        table.remove(qf_list, current_line_number)
+
+        vim.fn.setqflist(qf_list, "r")
+
+        local new_line_number = math.min(current_line_number, #qf_list)
+        vim.fn.cursor(new_line_number, 1)
+      end
+    end, {
+      buffer = true,
+      noremap = true,
+      silent = true,
+      desc = "Remove quickfix item under cursor",
+    })
+  end,
+  desc = "Attach keymaps for quickfix list",
+  group = general,
 })
--- lua/plugins/ai/opencode.lua
+++ lua/plugins/ai/opencode.lua
@@ -1,21 +1,32 @@
 ---@type LazySpec
 -- NOTE: Opencode
 return {
-  "sudo-tee/opencode.nvim",
-  enabled = false,
-  event = "VeryLazy",
-  opts = {
-    ui = {
-      input = {
-        text = {
-          wrap = true, -- Wraps text inside input window
+  {
+    "sudo-tee/opencode.nvim",
+    -- enabled = false,
+    event = "VeryLazy",
+    opts = {
+      ui = {
+        input = {
+          text = {
+            wrap = true, -- Wraps text inside input window
+          },
+        },
+      },
+      keymap_prefix = "<leader>a",
+      keymap = {
+        editor = {
+          ["<leader>aa"] = { "toggle" }, -- Open opencode. Close if opened
         },
       },
     },
-    keymap_prefix = "<leader>a",
-    keymap = {
-      editor = {
-        ["<leader>aa"] = { "toggle" }, -- Open opencode. Close if opened
+  },
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      spec = {
+        { "<leader>a", group = "AI" },
       },
     },
   },
