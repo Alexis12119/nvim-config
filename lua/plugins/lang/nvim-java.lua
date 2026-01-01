dffa47427c9725b35d553e728eed3ab348476ac6
Alexis12119 <corporal461@gmail.com>
2026-01-02 04:13:54 +0800

    feat(nvim-java): simplify and make spring-boot work.

--- lua/plugins/lang/nvim-java.lua
+++ lua/plugins/lang/nvim-java.lua
@@ -4,34 +4,10 @@ return {
   {
     "nvim-java/nvim-java",
     ft = { "java" },
-    dependencies = {
-      {
-        "neovim/nvim-lspconfig",
-        opts = {
-          servers = {
-            jdtls = {
-              -- Your custom jdtls settings goes here
-              capabilities = require("blink.cmp").get_lsp_capabilities(),
-            },
-          },
-          setup = {
-            jdtls = function()
-              require("java").setup({
-                -- Your custom nvim-java configuration goes here
-                spring_boot_tools = {
-                  enable = false,
-                  version = "1.63.0",
-                },
-                jdk = {
-                  auto_install = false,
-                  version = "21",
-                },
-              })
-            end,
-          },
-        },
-      },
-    },
+    config = function()
+      require("java").setup()
+      vim.lsp.enable("jdtls")
+    end,
   },
   {
     "mfussenegger/nvim-jdtls",
