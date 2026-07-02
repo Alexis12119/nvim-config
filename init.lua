e3f08375e3243fc7d8b009f5f9213b38cd13cff0
Alexis12119 <corporal461@gmail.com>
2026-07-02 20:56:09 +0800

    feat: don't auto open output panel in vscode.

--- init.lua
+++ init.lua
@@ -2,3 +2,8 @@ require("config.lazy")
 require("config.commands")
 require("config.filetypes")
 require("config.utils")
+
+if vim.g.vscode then
+  vim.opt.shortmess:append("s")
+  vim.opt.cmdheight = 1
+end
